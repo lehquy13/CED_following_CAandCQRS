de230564e0c8f6663839b8afc4cb3cae73665dae
Lê Hoàng Quý <91002362+lehquy13@users.noreply.github.com>
2023-04-26 12:04:08 +0700

    add validation query

---
+++ CED.Application/Services/Authentication/Queries/ValidateToken/ValidateTokenQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using CED.Contracts.Interfaces.Authentication;
+using MediatR;
+
+namespace CED.Application.Services.Authentication.Queries.ValidateToken;
+
+public class ValidateTokenQueryHandler
+    : IRequestHandler<ValidateTokenQuery, bool>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    
+    public ValidateTokenQueryHandler(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public async Task<bool> Handle(ValidateTokenQuery query, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        return _jwtTokenGenerator.ValidateToken(query.validateToken);
+    }
+}
+
---
+++ CED.Application/Services/Authentication/Queries/ValidateToken/ValidateTokenQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+namespace CED.Application.Services.Authentication.Queries.ValidateToken;
+
+public class ValidateTokenQueryValidator : AbstractValidator<ValidateTokenQuery>
+{
+    public ValidateTokenQueryValidator()
+    {
+        RuleFor(x => x.validateToken).NotEmpty();
+        
+    }
+}
+
