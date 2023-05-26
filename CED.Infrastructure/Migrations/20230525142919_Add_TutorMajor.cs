67f528e325e19cbd181d5888a04fd2f936157a3d
Lê Hoàng Quý <91002362+lehquy13@users.noreply.github.com>
2023-05-26 21:48:25 +0700

    add tutormigration

---
+++ CED.Infrastructure/Migrations/20230525142919_Add_TutorMajor.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CED.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_TutorMajor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserClassInformation");
+
+            migrationBuilder.RenameColumn(
+                name: "isVerified",
+                table: "User",
+                newName: "IsVerified");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "University",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "TutorMajor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TutorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutorMajor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TutorMajor_Subject_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subject",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TutorMajor_User_TutorId",
+                        column: x => x.TutorId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorMajor_SubjectId",
+                table: "TutorMajor",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorMajor_TutorId",
+                table: "TutorMajor",
+                column: "TutorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutorMajor");
+
+            migrationBuilder.RenameColumn(
+                name: "IsVerified",
+                table: "User",
+                newName: "isVerified");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "University",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UserClassInformation",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClassInformationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    isTutor = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClassInformation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserClassInformation_ClassInformation_ClassInformationId",
+                        column: x => x.ClassInformationId,
+                        principalTable: "ClassInformation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserClassInformation_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserClassInformation_ClassInformationId",
+                table: "UserClassInformation",
+                column: "ClassInformationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserClassInformation_UserId",
+                table: "UserClassInformation",
+                column: "UserId");
+        }
+    }
+}
