7591957247d65a7b2d566a8db2fb7ced5875167f
Lê Hoàng Quý <91002362+lehquy13@users.noreply.github.com>
2023-05-27 23:25:53 +0700

    .

---
+++ CED.Web.CustomerSide/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CED.Web.CustomerSide.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
