b5b6d6c5a875a7c15882718b3576ba632b22a814
Lê Hoàng Quý <91002362+lehquy13@users.noreply.github.com>
2023-06-08 22:02:11 +0700

    add more attrrs for requestclass

---
+++ CED.Infrastructure/Migrations/20230608110616_Add_Request_details.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CED.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Request_details : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "RequestGettingClass",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RequestStatus",
+                table: "RequestGettingClass",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "RequestGettingClass");
+
+            migrationBuilder.DropColumn(
+                name: "RequestStatus",
+                table: "RequestGettingClass");
+        }
+    }
+}
